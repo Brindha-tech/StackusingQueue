ea81abc012d1962f0d99201c3addfd1d9a40b425
Brindha S <brinda.tulsi@gmail.com>
2020-05-30 11:48:57 +0530

    First commit

---
+++ StackusingQueue/InvoiceRepository.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace StackusingQueue
+{
+    public class InvoiceRepository 
+    {
+
+        public static void Main(String [] args)
+        {
+            Invoice inv = new Invoice();
+            inv.Id = 23;
+            InvoiceItem i1 = new InvoiceItem();
+            i1.Price = 3;
+
+            InvoiceItem i2 = new InvoiceItem();
+            i2.Price = 5;
+            IList<InvoiceItem> items1 = new List<InvoiceItem>();
+            items1.Add(i1);
+            items1.Add(i2);
+            inv.InvoiceItems = items1;
+
+            Invoice inv3 = new Invoice();
+            inv3.Id = 33;
+
+            InvoiceItem i31 = new InvoiceItem();
+            i31.Price = 20;
+
+            InvoiceItem i32 = new InvoiceItem();
+            i32.Price = 30;
+            IList<InvoiceItem> items31 = new List<InvoiceItem>();
+            items31.Add(i31);
+            items31.Add(i32);
+            inv3.InvoiceItems = items31;
+
+            IList<Invoice> invoices = new List<Invoice>();
+            invoices.Add(inv);
+            invoices.Add(inv3);
+            InvoiceRepository repo = new InvoiceRepository(invoices.AsQueryable());
+            Console.WriteLine(repo.GetTotal(23));
+            Console.WriteLine("done");
+            Console.ReadLine();
+        }
+        IQueryable<Invoice> invoices = null;
+        public InvoiceRepository(IQueryable<Invoice> invoices)
+        {
+            this.invoices = invoices;
+        }
+        /// <summary>
+        /// Should return a total value of an invoice with a given id. If an invoice does not exist null should be returned.
+        /// </summary>
+        /// <param name="invoiceId"></param>
+        /// <returns></returns>
+        public decimal? GetTotal(int invoiceId)
+        {
+            var matchedInovice = invoices.Where(inv => inv.Id.Equals(invoiceId));
+            if(matchedInovice.Any())
+            {
+                return matchedInovice.First().InvoiceItems.Select(invItem => invItem.Price).Sum();
+            }
+            return null;
+
+        }
+        /// <summary>
+        /// Should return a total value of all unpaid invoices.
+        /// </summary>
+        /// <returns></returns>
+        public decimal GetTotalOfUnpaid()
+        {
+           return invoices.Where(inv => inv.AcceptanceDate == null).Select(
+                               inv => inv.InvoiceItems.Select(invItem => invItem.Price).Sum()).Sum();
+        }
+        /// <summary>
+        /// Should return a dictionary where the name of an invoice item is a key and the number of bought items is a value.
+        /// The number of bought items should be summed within a given period of time (from, to). Both the from date and the end date can be null.
+        /// </summary>
+        /// <param name="from"></param>
+        /// <param name="to"></param>
+        /// <returns></returns>
+        public IReadOnlyDictionary<string, long> GetItemsReport(DateTime? from, DateTime? to)
+        {
+            Dictionary<string, long> itemsCount = new Dictionary<string, long>();
+            IEnumerable<Invoice> matchedInvoices = invoices.Where(invoice => liesIntheRange(from, to, invoice.CreationDate)).ToList();
+
+            foreach (Invoice inv in matchedInvoices)
+            {
+                foreach (InvoiceItem item in  inv.InvoiceItems)
+                {
+                    itemsCount[item.Name] = itemsCount.GetValueOrDefault(item.Name, 0);
+                }
+            }
+            return itemsCount;
+
+            //.ToDictionary(inv => inv.Description,
+
+            //    inv => (long) inv.InvoiceItems.Count);
+            
+            
+        }
+        private static bool liesIntheRange(DateTime? from, DateTime? to, DateTime input)
+        {
+            if (from != null && from > input)
+                return false;
+            else if (to != null && to < input)
+                return false;
+            return true;
+        }
+    }
+}
+
+public class Invoice
+{
+    // A unique numerical identifier of an invoice (mandatory)
+    public int Id { get; set; }
+    // A short description of an invoice (optional).
+    public string Description { get; set; }
+    // A number of an invoice e.g. 134/10/2018 (mandatory).
+    public string Number { get; set; }
+    // An issuer of an invoice e.g. Metz-Anderson, 600  Hickman Street,Illinois (mandatory).
+    public string Seller { get; set; }
+    // A buyer of a service or a product e.g. John Smith, 4285  Deercove Drive, Dallas (mandatory).
+    public string Buyer { get; set; }
+    // A date when an invoice was issued (mandatory).
+    public DateTime CreationDate { get; set; }
+    // A date when an invoice was paid (optional).
+    public DateTime? AcceptanceDate { get; set; }
+    // A collection of invoice items for a given invoice (can be empty but is never null).
+    public IList<InvoiceItem> InvoiceItems { get; set; }
+    public Invoice()
+    {
+        InvoiceItems = new List<InvoiceItem>();
+    }
+}
+public class InvoiceItem
+{
+    // A name of an item e.g. eggs.
+    public string Name { get; set; }
+    // A number of bought items e.g. 10.
+    public uint Count { get; set; }
+    // A price of an item e.g. 20.5.
+    public decimal Price { get; set; }
+}
+
---
+++ StackusingQueue/Program.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading;
+
+namespace StackusingQueue
+{
+    class Program
+    {
+        //public class StackImplementation
+        //{
+
+        //IMPLEMENT STACK USING QUEUE
+        //Queue<int> Que = new Queue<int>();
+
+        //public void Push(int value)
+        //{
+        //    Que.Enqueue(value);
+        //    Console.WriteLine("Pushed {0}", value);
+        //}
+
+        //public int pop()
+        //{
+        //    if (Que.Count == 0)
+        //    {
+        //        Console.WriteLine("Stack is empty");
+        //        return -1;
+        //    }
+        //    else
+        //    {
+        //        int counter = Que.Count;
+        //        while (counter > 1)
+        //        {
+        //            Que.Enqueue(Que.Dequeue());
+        //            counter--;
+        //        }                    
+        //        return Que.Dequeue();
+        //    }
+        //}
+
+        //IMPLEMENT QUEUE USING STACK
+        //Stack<int> stk = new Stack<int>();
+        //int[] arr = new int[30];
+
+        //public void Enqueue(int value) {
+        //    stk.Push(value);
+        //    Console.WriteLine("Enqueued {0}", value);
+        //}
+
+        //public int Dequeue()
+        //{
+        //    if (stk.Count == 0)
+        //        return -1;
+        //    else
+        //    {
+        //        int counter = stk.Count - 1;
+        //        int c1 = 0;
+        //        for (int i=0; i<counter; i++)
+        //        {
+        //            arr[i] = stk.Pop();
+        //            c1++;
+        //        }
+        //        int poppedValue = stk.Pop();
+        //        for(int j = c1-1; j>=0; j--)
+        //        {
+        //            stk.Push(arr[j]);
+        //        }
+        //        return poppedValue;
+        //    }
+        //}
+
+
+
+        //}
+        //static void Main(string[] args)
+        //{
+        //    StackImplementation s = new StackImplementation();
+        //    s.Enqueue(5);
+        //    s.Enqueue(4);
+        //    s.Enqueue(3);
+        //    s.Enqueue(2);
+        //    s.Enqueue(1);
+        //    int i = s.Dequeue();
+        //    Console.WriteLine("Dequeuing {0}", i);
+        //    int j = s.Dequeue();
+        //    Console.WriteLine("Dequeuing {0}", j);
+        //    Console.ReadKey();
+        //}
+
+        int i = 0;
+        static void Main2(string[] args)
+        {
+
+            //Thread t = new Thread(childFn);
+            //t.Start();
+
+            //t.Join();
+            ThreadSample sample = new ThreadSample();
+            Thread producer = new Thread(sample.Producer);
+            //Thread producer = new Thread(() =>
+            //   {
+            //       while (true)
+            //       {
+            //           //Thread.Sleep(1000);
+            //           sample.Producer();
+            //       }
+            //   });
+
+            Thread consumer = new Thread(() =>
+            {
+                while (true)
+                {
+                    //Thread.Sleep(1000);
+                    sample.Consumer();
+                }
+            });
+
+            producer.Start();
+            consumer.Start();
+
+            producer.Join();
+            consumer.Join();
+            Console.WriteLine("Hi Everyone");
+            Console.ReadLine();
+        }
+
+        static void childFn()
+        {
+
+            Console.WriteLine("child start");
+            Thread.Sleep(100);
+
+
+            Console.WriteLine("child end");
+        }
+
+
+        public class ThreadSample
+        {
+
+            int current = 0;
+            static int size=10;
+            private readonly object _lock = new object();
+            static Mutex mutex = new Mutex();
+            public void Producer()
+            {                
+                lock (_lock)
+                {
+                    while (current == size)
+                    {
+                        Console.WriteLine("Producer waiting");
+                        Monitor.Wait(_lock);
+                    } 
+                    current++;
+                    Console.WriteLine("Producer {0}", current);
+                    Monitor.Pulse(_lock);
+                }
+            }
+            public void Consumer()
+            {
+                lock (_lock) 
+                {
+                    while(current == 0)
+                    {
+                        Console.WriteLine("Consumer waiting");
+
+                        Monitor.Wait(_lock);
+                    }
+                    Console.WriteLine("Consumer took {0}", current);
+                    current--;
+                    Monitor.Pulse(_lock);
+                }
+            }
+
+        }
+    }       
+
+}
